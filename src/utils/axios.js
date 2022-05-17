56148beabb507e230fbe643caff37d108b6e4ceb
unknown <fernandaaflah@gmail.com>
2022-05-17 09:42:47 +0700

    env

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosApiIntances = axios.create({
-  baseURL: "http://localhost:3001/"
+  // baseURL: "http://localhost:3001/"
+  baseURL: process.env.baseURL
 });
 // Add a request interceptor
 axiosApiIntances.interceptors.request.use(
